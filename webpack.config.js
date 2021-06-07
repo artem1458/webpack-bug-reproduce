de8e9e44574e89b8c0318fd7e9ab8846f97a9894
Artem Kornev <artemkakornev1@gmail.com>
2021-06-07 14:01:12 +0300

    master

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const RebuildNonDirectlyRelatedModulesPlugin = require('./RebuildNonDirectlyRelatedModulesPlugin')
-const BabelPlugin = require('./BabelPlugin')
 
 module.exports = {
     entry: './src/index.ts',
